79c6f9d1a7a544f6e647b202409c835979573f21
Sergio <sergio.arango63723@ucaldas.edu.co>
2026-06-14 23:59:51 -0500

    Cambios visuales acordes a mockups

--- src/app/layouts/full/sidebar/branding.component.ts
+++ src/app/layouts/full/sidebar/branding.component.ts
@@ -7,7 +7,7 @@ import { CoreService } from 'src/app/services/core.service';
   template: `
     <a href="/" class="logodark">
       <img
-        src="./assets/images/logos/dark-logo.svg"
+        src="./assets/images/logos/dark-logo.png"
         class="align-middle m-2"
         alt="logo"
       />
@@ -15,7 +15,7 @@ import { CoreService } from 'src/app/services/core.service';
 
     <a href="/" class="logolight">
       <img
-        src="./assets/images/logos/light-logo.svg"
+        src="./assets/images/logos/light-logo.png"
         class="align-middle m-2"
         alt="logo"
       />
